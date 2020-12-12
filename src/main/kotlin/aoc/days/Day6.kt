7b604cf657b1980395b47c86eebe2857557eeb6f
Cliffred van Velzen <cliffred@cliffred.nl>
2020-12-12 14:03:03 +0100

    Day 6

---
+++ src/main/kotlin/aoc/days/Day6.kt
@@ -0,0 +1,56 @@
+package aoc.days
+
+import aoc.model.Day
+
+fun main() {
+    Day6().solve()
+}
+
+class Day6 : Day() {
+    override fun part1() = answers1(input()).map { it.size }.sum().toLong()
+
+    override fun part2() = answers2(input()).map { it.size }.sum().toLong()
+}
+
+fun answers1(lines: Sequence<String>): List<Set<Char>> {
+    val answerPerGroup = mutableListOf<Set<Char>>()
+    var answers = mutableSetOf<Char>()
+    lines.forEach { line ->
+        if (line.isBlank()) {
+            answerPerGroup += answers
+            answers = mutableSetOf()
+        } else {
+            answers.addAll(line.toCharArray().asIterable())
+        }
+    }
+    if (answers.isNotEmpty()) {
+        answerPerGroup += answers
+    }
+
+    return answerPerGroup
+}
+
+fun answers2(lines: Sequence<String>): List<Set<Char>> {
+    val answerPerGroup = mutableListOf<Set<Char>>()
+    var answers = mutableSetOf<Char>()
+    var newGroup = true
+    lines.forEach { line ->
+        if (line.isBlank()) {
+            answerPerGroup += answers
+            answers = mutableSetOf()
+            newGroup = true
+        } else {
+            if (newGroup) {
+                answers.addAll(line.toCharArray().asIterable())
+                newGroup = false
+            } else {
+                answers.removeIf { it !in line.toCharArray() }
+            }
+        }
+    }
+    if (answers.isNotEmpty()) {
+        answerPerGroup += answers
+    }
+
+    return answerPerGroup
+}
