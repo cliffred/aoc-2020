b90e826ab64aa25feeae0172690b3e35d6d97a81
Cliffred van Velzen <cliffred@cliffred.nl>
2020-12-12 14:01:57 +0100

    Basic gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+    id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
+}
+
+group = "red.cliff"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
