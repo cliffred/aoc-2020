c8c11882b8f7dc544fdede6bbfe6c3ebf4b9b2be
Cliffred van Velzen <cliffred@cliffred.nl>
2020-12-19 13:26:56 +0100

    Day 18 Part 2

--- src/main/kotlin/aoc/days/Day18.kt
+++ src/main/kotlin/aoc/days/Day18.kt
@@ -9,51 +9,55 @@ fun main() {
 class Day18 : Day() {
     private val inputList = input().toList()
 
-    override fun part1() = inputList.map { calc(it) }.sum()
+    override fun part1() = inputList.map { calc(it, ::simpleSolve) }.sum()
 
-    override fun part2() = TODO()
+    override fun part2() = inputList.map { calc(it, ::plusFirstSolve) }.sum()
 }
 
-private fun calc(sum: String): Long {
-    if ('(' !in sum) {
-        return simpleSolve(sum)
+private fun calc(equation: String, secondSolve: (String) -> Long): Long {
+    if ('(' !in equation) {
+        return secondSolve(equation)
     }
-    val a = sum.lastIndexOf('(')
-    val b = sum.indexOf(')', a)
-    val sub = calc(sum.substring(a + 1, b))
-    return calc(sum.substring(0, a) + sub + sum.substring(b + 1))
+    val a = equation.lastIndexOf('(')
+    val b = equation.indexOf(')', a)
+    val sub = calc(equation.substring(a + 1, b), secondSolve)
+    return calc(equation.substring(0, a) + sub + equation.substring(b + 1), secondSolve)
 }
 
-private fun simpleSolve(sum: String): Long {
+private fun plusFirstSolve(equation: String): Long {
+    if (('+' in equation).xor('*' in equation)) {
+        return simpleSolve(equation)
+    }
+    val plusIdx = equation.indexOf('+')
+    val firstNum = equation.substring(0, plusIdx - 1).substringAfterLast(' ')
+    val secondNum = equation.substring(plusIdx + 2).takeWhile { it.isDigit() }
+    val plusSum = "$firstNum + $secondNum"
+    val ans = simpleSolve(plusSum)
+    return plusFirstSolve(equation.replaceFirst(plusSum, ans.toString()))
+}
+
+private fun simpleSolve(equation: String): Long {
     var acc = 0L
     var lastOperator = Operator.PLUS
-    sum.split(' ').forEach { expr ->
+    equation.split(' ').forEach { expr ->
         if (expr.toLongOrNull() != null) {
             acc = lastOperator(acc, expr.toLong())
         } else {
             when (expr) {
                 "+" -> lastOperator = Operator.PLUS
-                "-" -> lastOperator = Operator.MINUS
                 "*" -> lastOperator = Operator.TIMES
-                "/" -> lastOperator = Operator.DIV
             }
         }
     }
     return acc
 }
 
-enum class Operator {
+private enum class Operator {
     PLUS {
         override fun invoke(a: Long, b: Long) = a + b
     },
-    MINUS {
-        override fun invoke(a: Long, b: Long) = a - b
-    },
     TIMES {
         override fun invoke(a: Long, b: Long) = a * b
-    },
-    DIV {
-        override fun invoke(a: Long, b: Long) = a / b
     };
 
     abstract operator fun invoke(a: Long, b: Long): Long
